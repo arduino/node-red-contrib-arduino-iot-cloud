2d593ae0caff477195c3455438228143faac0898
fabio <fabiomassimo.centonze@gmail.com>
2019-10-21 12:14:03 +0200

    using arduino-iot-client-js lib for api

--- arduino-cloud-api.js
+++ arduino-cloud-api.js
@@ -20,10 +20,18 @@
 'use strict';
 
 const request = require('request');
-const envUrl =
-  process.env.IOT_API_BASE_URL || 'https://api-dev.arduino.cc/iot/v1'; 
+const ArduinoIotClient = require('@arduino/arduino-iot-client');
+const client = ArduinoIotClient.ApiClient.instance;
+//Only for dev
+client.basePath='http://api-dev.arduino.cc/iot';
+// Configure OAuth2 access token for authorization: oauth2
+var oauth2 = client.authentications['oauth2'];
+const apiProperties = new ArduinoIotClient.PropertiesV2Api(client);
+const apiSeries = new ArduinoIotClient.SeriesV2Api(client);
+const apiThings = new ArduinoIotClient.ThingsV2Api(client);
+
 const authUrl =
-  process.env.AUTH_API_BASE_URL || 'https://auth-dev.arduino.cc/v1'; 
+  process.env.AUTH_API_BASE_URL || 'https://auth-dev.arduino.cc/v1';
 
 function log(title, msg) {
   console.log(`[${title}] ${msg}`);
@@ -31,35 +39,33 @@ function log(title, msg) {
 class ArduinoCloudClient {
   constructor(token) {
     this.token = token;
+    oauth2.accessToken = token;
   }
   updateToken(token) {
     this.token = token;
+    oauth2.accessToken = token;
   }
   getUserId() {
     const url = `${authUrl}/users/byID/me`;
     return this.genericRequest(url, 'get', '');
   }
   setProperty(thing_id, property_id, value) {
-    const url = `${envUrl}/things/${thing_id}/properties/${property_id}/publish`;
     const body = JSON.stringify({
       value: value
     });
-    return this.genericRequest(url, 'put', body);
+    return apiProperties.propertiesV2Publish(thing_id, property_id, body);
   }
   getThings() {
-    const url = `${envUrl}/things`;
-    return this.genericRequest(url, 'get', '');
+    return apiThings.thingsV2List();
   }
   getProperties(thingId) {
-    const url = `${envUrl}/things/${thingId}/properties`;
-    return this.genericRequest(url, 'get', '');
+    return apiProperties.propertiesV2List(thingId);
   }
   getProperty(thingId, propertyId) {
-    const url = `${envUrl}/things/${thingId}/properties/${propertyId}`;
-    return this.genericRequest(url, 'get', '');
+    return apiProperties.propertiesV2Show(thingId, propertyId);
   }
   getSeries(thingId, propertyId, start, end) {
-    const url = `${envUrl}/series/batch_query_raw`;
+
     const body =  JSON.stringify({
       requests: [{
         q: "property." + propertyId,
@@ -68,9 +74,9 @@ class ArduinoCloudClient {
         sort: "ASC",
         series_limit: 86400
       }],
-      resp_version: 1  
+      resp_version: 1
     });
-    return this.genericRequest(url, 'post', body);
+    return apiSeries.seriesV2BatchQueryRaw(body);
   }
   genericRequest(url, method, body) {
     const p = new Promise((resolve, reject) => {
--- arduino-cloud.js
+++ arduino-cloud.js
@@ -1,4 +1,3 @@
-const moment = require("moment");
 const connectionManager = require("./arduino-connection-manager");
 
 module.exports = function(RED) {
@@ -14,35 +13,34 @@ module.exports = function(RED) {
           this.thing = config.thing;
           this.propertyId = config.property;
           this.propertyName = config.name;
-          this.poll();
+          this.poll(connectionConfig);
         }
       } catch (err) {
         console.log(err);
-        throw new Error(err);
       }
     }
     realConstructor.apply(this, [config]);
   }
   ArduinoIotInput.prototype = {
-    poll: async function() {
+    poll: async function(connectionConfig) {
       try {
+        await connectionManager.connect(connectionConfig);
         const property = await this.arduinoRestClient.getProperty(this.thing, this.propertyId);
-        if (property.last_value != this.lastValue) {
+        if (property.last_value !== this.lastValue) {
           const timestamp = (new Date()).getTime();
-          this.send(
-            {
-              topic: this.propertyName,
-              payload: property.last_value,
-              timestamp: timestamp
-            }            
-          );
+            this.send(
+              {
+                topic: this.propertyName,
+                payload: property.last_value,
+                timestamp: timestamp
+              }
+            );
           this.lastValue = property.last_value;
-        }      
-        
-        this.pollTimeout = setTimeout(() => { this.poll()}, 1000);
+        }
+
+        this.pollTimeout = setTimeout(() => { this.poll(connectionConfig)}, 1000);
       } catch (err) {
         console.log(err);
-        throw new Error(err);
       }
     }
   }
@@ -60,12 +58,15 @@ module.exports = function(RED) {
           this.propertyId = config.property;
           this.propertyName = config.name;
           this.on('input', function(msg) {
-            this.arduinoRestClient.setProperty(this.thing, this.propertyId, msg.payload);
+            try{
+              this.arduinoRestClient.setProperty(this.thing, this.propertyId, msg.payload);
+            } catch(err){
+              console.log(err);
+            }
           });
         }
       } catch (err) {
         console.log(err);
-        throw new Error(err);
       }
     }
     realConstructor.apply(this, [config]);
@@ -92,7 +93,6 @@ module.exports = function(RED) {
       return res.send(JSON.stringify(things));
     } catch (err) {
       console.log(err);
-      throw new Error(err);
     }
   });
 
@@ -109,7 +109,6 @@ module.exports = function(RED) {
       return res.send(JSON.stringify(properties));
     } catch (err) {
       console.log(err);
-      throw new Error(err);
     }
   });
 }
