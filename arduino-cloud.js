bcc8737f4a978417f36d11042e5fc158cb1424fb
ilcato <ilcato@yahoo.com>
2019-11-19 10:12:58 +0100

    Added additonal design time and runtime controls

--- arduino-cloud.js
+++ arduino-cloud.js
@@ -2,34 +2,34 @@ const connectionManager = require("./arduino-connection-manager");
 const moment = require("moment");
 const _ = require('lodash');
 
-module.exports = function(RED) {
+module.exports = function (RED) {
   function ArduinoIotInput(config) {
     const realConstructor = async (config) => {
       RED.nodes.createNode(this, config);
       const connectionConfig = RED.nodes.getNode(config.connection);
       this.lastValue = undefined;
-      try {
-        await connectionManager.connect(connectionConfig);
-        if (config.thing !== "" && config.property !== "") {
+      if (config.thing !== "" && config.thing !== "0" && config.property !== "" && config.property !== "0") {
+        try {
+          await connectionManager.connect(connectionConfig);
           this.arduinoRestClient = connectionManager.apiRest;
           this.thing = config.thing;
           this.propertyId = config.property;
           this.propertyName = config.name;
           this.poll(connectionConfig);
+        } catch (err) {
+          console.log(err);
         }
-      } catch (err) {
-        console.log(err);
       }
     }
     realConstructor.apply(this, [config]);
   }
   ArduinoIotInput.prototype = {
-    poll: async function(connectionConfig) {
+    poll: async function (connectionConfig) {
       try {
-		    await connectionManager.connect(connectionConfig);
+        await connectionManager.connect(connectionConfig);
         const property = await this.arduinoRestClient.getProperty(this.thing, this.propertyId);
         if (typeof (property.last_value) !== "object" && property.last_value !== this.lastValue ||
-            typeof (property.last_value) === "object" && !_.isEqual(property.last_value, this.lastValue)
+          typeof (property.last_value) === "object" && !_.isEqual(property.last_value, this.lastValue)
         ) {
           this.send(
             {
@@ -40,13 +40,13 @@ module.exports = function(RED) {
           );
           const s = getStatus(property.last_value);
           if (s)
-              this.status({fill: "grey",shape: "dot",text: s});
+            this.status({ fill: "grey", shape: "dot", text: s });
           else
             this.status({});
           this.lastValue = property.last_value;
         }
 
-        this.pollTimeout = setTimeout(() => { this.poll(connectionConfig)}, 1000);
+        this.pollTimeout = setTimeout(() => { this.poll(connectionConfig) }, 1000);
       } catch (err) {
         console.log(err);
       }
@@ -58,29 +58,31 @@ module.exports = function(RED) {
     const realConstructor = async (config) => {
       RED.nodes.createNode(this, config);
       const connectionConfig = RED.nodes.getNode(config.connection);
-      try {
-        await connectionManager.connect(connectionConfig);
-        if (config.thing !== "" && config.property !== "") {
-          this.arduinoRestClient = connectionManager.apiRest;
-          this.thing = config.thing;
-          this.propertyId = config.property;
-          this.propertyName = config.name;
-          this.on('input', async function(msg) {
-			      try{
-              await connectionManager.connect(connectionConfig);
-              this.arduinoRestClient.setProperty(this.thing, this.propertyId, msg.payload);
-              const s = getStatus(msg.payload);
-              if (s)
-                  this.status({fill: "grey",shape: "dot",text: s});
-              else
-                this.status({});
-            } catch(err){
-              console.log(err);
-            }
-          });
+      if (config.thing !== "" && config.thing !== "0" && config.property !== "" && config.property !== "0") {
+        try {
+          await connectionManager.connect(connectionConfig);
+          if (config.thing !== "" && config.property !== "") {
+            this.arduinoRestClient = connectionManager.apiRest;
+            this.thing = config.thing;
+            this.propertyId = config.property;
+            this.propertyName = config.name;
+            this.on('input', async function (msg) {
+              try {
+                await connectionManager.connect(connectionConfig);
+                this.arduinoRestClient.setProperty(this.thing, this.propertyId, msg.payload);
+                const s = getStatus(msg.payload);
+                if (s)
+                  this.status({ fill: "grey", shape: "dot", text: s });
+                else
+                  this.status({});
+              } catch (err) {
+                console.log(err);
+              }
+            });
+          }
+        } catch (err) {
+          console.log(err);
         }
-      } catch (err) {
-        console.log(err);
       }
     }
     realConstructor.apply(this, [config]);
@@ -94,22 +96,23 @@ module.exports = function(RED) {
       const node = this;
       this.timeWindowCount = config.timeWindowCount;
       this.timeWindowUnit = config.timeWindowUnit;
-      try{
-        this.arduinoRestClient = connectionManager.apiRest;
-        if (config.thing !== "" && config.property !== "") {
-          this.thing = config.thing;
-          this.propertyId = config.property;
-          this.propertyName = config.name;
-          node.on('input', async function() {
-            const now = moment();
-            const end = now.format();
-            const start = now.subtract(this.timeWindowCount * this.timeWindowUnit, 'second').format();
-            await connectionManager.connect(connectionConfig);
-            const result = await this.arduinoRestClient.getSeries(this.thing, this.propertyId,start,end);
-            const times= result.responses[0].times;
-            const values = result.responses[0].values;
-            let data = [];
-              if(values && times) {
+      if (config.thing !== "" && config.thing !== "0" && config.property !== "" && config.property !== "0") {
+        try {
+          this.arduinoRestClient = connectionManager.apiRest;
+          if (config.thing !== "" && config.property !== "") {
+            this.thing = config.thing;
+            this.propertyId = config.property;
+            this.propertyName = config.name;
+            node.on('input', async function () {
+              const now = moment();
+              const end = now.format();
+              const start = now.subtract(this.timeWindowCount * this.timeWindowUnit, 'second').format();
+              await connectionManager.connect(connectionConfig);
+              const result = await this.arduinoRestClient.getSeries(this.thing, this.propertyId, start, end);
+              const times = result.responses[0].times;
+              const values = result.responses[0].values;
+              let data = [];
+              if (values && times) {
                 values.forEach(function (item, index, array) {
                   data.push({
                     x: moment(times[index]).unix() * 1000,
@@ -117,19 +120,20 @@ module.exports = function(RED) {
                   });
                 });
               }
-            node.send(
-              {
-                topic: config.name,
-                payload: [{
-                  series: [],
-                  data: [data]
-                }]
-              }
-            );
-          });
+              node.send(
+                {
+                  topic: config.name,
+                  payload: [{
+                    series: [],
+                    data: [data]
+                  }]
+                }
+              );
+            });
+          }
+        } catch (err) {
+          console.log(err);
         }
-      }catch(err){
-        console.log(err);
       }
     }
     realConstructor.apply(this, [config]);
@@ -140,43 +144,43 @@ module.exports = function(RED) {
     const realConstructor = async (config) => {
       RED.nodes.createNode(this, config);
       const connectionConfig = RED.nodes.getNode(config.connection);
-      const node = this;
-
       this.timeWindowCount = config.timeWindowCount;
       this.timeWindowUnit = config.timeWindowUnit;
-      try{
-        this.arduinoRestClient = connectionManager.apiRest;
-        if (config.thing !== "" && config.property !== "") {
-        this.thing = config.thing;
-        this.propertyId = config.property;
-        this.propertyName = config.name;
-        const pollTime = this.timeWindowCount * this.timeWindowUnit;
-        this.poll(connectionConfig, pollTime);
+      if (config.thing !== "" && config.thing !== "0" && config.property !== "" && config.property !== "0") {
+        try {
+          this.arduinoRestClient = connectionManager.apiRest;
+          if (config.thing !== "" && config.property !== "") {
+            this.thing = config.thing;
+            this.propertyId = config.property;
+            this.propertyName = config.name;
+            const pollTime = this.timeWindowCount * this.timeWindowUnit;
+            this.poll(connectionConfig, pollTime);
+          }
+        } catch (err) {
+          console.log(err);
         }
-      }catch(err){
-        console.log(err);
       }
     }
     realConstructor.apply(this, [config]);
   }
   ArduinoIotInputPoll.prototype = {
-    poll: async function(connectionConfig, pollTime) {
+    poll: async function (connectionConfig, pollTime) {
       try {
         await connectionManager.connect(connectionConfig);
         const property = await this.arduinoRestClient.getProperty(this.thing, this.propertyId);
-          this.send(
-            {
-              topic: property.name,
-              payload: property.last_value,
-              timestamp: property.value_updated_at
-            }
-          );
-          const s = getStatus(property.last_value);
-          if (s)
-              this.status({fill: "grey",shape: "dot",text: s});
-          else
-            this.status({});
-        this.pollTimeoutPoll = setTimeout(() => { this.poll(connectionConfig, pollTime)}, pollTime*1000);
+        this.send(
+          {
+            topic: property.name,
+            payload: property.last_value,
+            timestamp: property.value_updated_at
+          }
+        );
+        const s = getStatus(property.last_value);
+        if (s)
+          this.status({ fill: "grey", shape: "dot", text: s });
+        else
+          this.status({});
+        this.pollTimeoutPoll = setTimeout(() => { this.poll(connectionConfig, pollTime) }, pollTime * 1000);
       } catch (err) {
         console.log(err);
       }
@@ -190,32 +194,34 @@ module.exports = function(RED) {
       RED.nodes.createNode(this, config);
       const connectionConfig = RED.nodes.getNode(config.connection);
       const node = this;
-      try {
-        await connectionManager.connect(connectionConfig);
-        if (config.thing !== "" && config.property !== "") {
-          this.arduinoRestClient = connectionManager.apiRest;
-          this.thing = config.thing;
-          this.propertyId = config.property;
-          this.propertyName = config.name;
-          node.on('input', async function() {
-            await connectionManager.connect(connectionConfig);
-            const property = await this.arduinoRestClient.getProperty(this.thing, this.propertyId);
-            this.send(
-              {
-                topic: property.name,
-                payload: property.last_value,
-                timestamp: property.value_updated_at
-              }
-            );
-            const s = getStatus(property.last_value);
-            if (s)
-                this.status({fill: "grey",shape: "dot",text: s});
-            else
-              this.status({});
+      if (config.thing !== "" && config.thing !== "0" && config.property !== "" && config.property !== "0") {
+        try {
+          await connectionManager.connect(connectionConfig);
+          if (config.thing !== "" && config.property !== "") {
+            this.arduinoRestClient = connectionManager.apiRest;
+            this.thing = config.thing;
+            this.propertyId = config.property;
+            this.propertyName = config.name;
+            node.on('input', async function () {
+              await connectionManager.connect(connectionConfig);
+              const property = await this.arduinoRestClient.getProperty(this.thing, this.propertyId);
+              this.send(
+                {
+                  topic: property.name,
+                  payload: property.last_value,
+                  timestamp: property.value_updated_at
+                }
+              );
+              const s = getStatus(property.last_value);
+              if (s)
+                this.status({ fill: "grey", shape: "dot", text: s });
+              else
+                this.status({});
             });
+          }
+        } catch (err) {
+          console.log(err);
         }
-      } catch (err) {
-        console.log(err);
       }
     }
     realConstructor.apply(this, [config]);
@@ -224,14 +230,14 @@ module.exports = function(RED) {
   RED.nodes.registerType("property in push", ArduinoIotInputPush);
 
   function ArduinoConnectionNode(config) {
-    RED.nodes.createNode(this,config);
+    RED.nodes.createNode(this, config);
     this.applicationname = config.applicationname;
     this.clientid = config.clientid;
     this.clientsecret = config.clientsecret;
   }
-  RED.nodes.registerType("arduino-connection",ArduinoConnectionNode);
+  RED.nodes.registerType("arduino-connection", ArduinoConnectionNode);
 
-  RED.httpAdmin.get("/things", RED.auth.needsPermission('Property-in.read'), async function(req,res) {
+  RED.httpAdmin.get("/things", RED.auth.needsPermission('Property-in.read'), async function (req, res) {
     try {
       const connectionConfig = {
         clientid: req.query.clientid,
@@ -246,7 +252,7 @@ module.exports = function(RED) {
     }
   });
 
-  RED.httpAdmin.get("/properties", RED.auth.needsPermission('Property-in.read'), async function(req,res) {
+  RED.httpAdmin.get("/properties", RED.auth.needsPermission('Property-in.read'), async function (req, res) {
     try {
       const connectionConfig = {
         clientid: req.query.clientid,
@@ -267,7 +273,7 @@ module.exports = function(RED) {
 }
 
 function getStatus(value) {
-	if (typeof value !== "object") {
+  if (typeof value !== "object") {
     if (typeof value === "number" && !(Number.isInteger(value)))
       return value.toFixed(3);
     else
